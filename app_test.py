f6d258048371c3a33d282343afd8e5ae2f4065b3
Prakshal Jain <prakshalj0512@gmail.com>
2020-09-30 23:24:16 -0700

    new dag

---
+++ app_test.py
@@ -0,0 +1,27 @@
+import airflow
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.models import Variable
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+
+args = {
+    'owner': 'Prakshal Jain',
+    'start_date': datetime(2020, 7, 7, 15, 1, 0),
+    'retries': 0,
+    'retry_delay': timedelta(seconds=60),
+    'email': "prakshal.jain@clairvoyantsoft.com",
+    'email_on_retry': False,
+    'email_on_failure': True,
+}
+dag = DAG(
+    dag_id='app_test', default_args=args,
+    schedule_interval=None,
+    catchup=False,
+    max_active_runs=1,
+)
+
+
+bash_operator = BashOperator(
+    task_id='role', bash_command="exit 0", dag=dag)
